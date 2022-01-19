cdd5b3872910069674679bcd54c8498a29c1eb87
equarmx <equarmx@gmail.com>
2022-01-19 19:31:52 +0500

    starting vuex and data

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    peopleList: []
   },
   mutations: {
+    setPeopleList(state, arr) {
+      state.peopleList = []
+      state.peopleList = arr
+    }
   },
   actions: {
+    // async set_People_list({commit}) {
+    //
+    // }
   },
-  modules: {
-  }
 })
